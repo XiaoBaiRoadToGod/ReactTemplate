f038747f7d564fbd44b855eed38c031ff6c5acf5
ZjVirus <285857527@qq.com>
2018-05-21 13:56:20 +0800

    添加webpack插件

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -13,6 +13,7 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const colors = require('./theme-color');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -254,6 +255,9 @@ module.exports = {
     ]
   },
   plugins: [
+    // 打包文件分析
+
+    new BundleAnalyzerPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
